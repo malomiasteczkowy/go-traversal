6366c0b98e037fff4d5bef03f194bc2a492377bd
Malomiasteczkowy <dr_oetker@o2.pl>
2018-11-06 14:40:24 +0100

    waitgroup-stats: using atomic/sync to update shared counter

--- go-practices/waitgroup-stats.go
+++ go-practices/waitgroup-stats.go
@@ -5,67 +5,61 @@ import (
 	"sync"
 	"time"
 	"math/rand"
+	"sync/atomic"
 )
 
 /* WaitGroup is a kind of a semaphore in its behaviour */
 
 /* should use sync/atomic */
 
-type RetStat struct{
-	retcode int
-	count int
-}
+const MAXRETCODE int = 1000
+const THREADCOUNT int = 100
 
 type Stat struct{
-	in	int
-	retcodes []*RetStat
+	in	uint64
+	retcodes [MAXRETCODE]uint64
+	other uint64
 }
 
+/* thread unsafe */
 func printStat(s *Stat){
-	fmt.Printf("Total: %d\n", s.in)
-	for _,rs := range s.retcodes{
-		fmt.Printf("[%d]:%d\n", rs.retcode, rs.count)
-	}
-}
-
-func updateStat(s *Stat, chs chan *Stat, inRetcode int){
-	s.in++
-	
-	total:= <- chs.in
-	chs.in <- total+1
-
-	for _,rs := range s.retcodes{
-		if rs.retcode == inRetcode{
-			rs.count++
-			return
+	total:=s.in
+	fmt.Printf("Total: %d\n", total)
+	for i,v := range s.retcodes{
+		if v>0{
+			fmt.Printf("[%d]: %2d\n", i, v)	
 		}
 	}
+	fmt.Printf("other: %d\n", s.other)
+}
 
-	s.retcodes = append(s.retcodes, new(RetStat))
-	last := len(s.retcodes)
-	s.retcodes[last-1].retcode = inRetcode
-	s.retcodes[last-1].count++
+/* thread safe - using sync/atomic */
+func updateStat(s *Stat, inRetCode int){
+	atomic.AddUint64(&(s.in), 1)
+	if inRetCode < MAXRETCODE {
+		atomic.AddUint64(&(s.retcodes[inRetCode]), 1)
+	} else {
+		atomic.AddUint64(&(s.other), 1)
+	}
 }
 
 func main(){
 
 	var wg sync.WaitGroup
-	respond := make(chan string, 5)
+	respond := make(chan string, THREADCOUNT)
 
 	/* Stat init */
 	stat := new(Stat)
-	chstat := make(chan *Stat, 5)
 	
-	// set semaphore to 5
-	wg.Add(5)
+	fmt.Printf("Starting %d tasks\n", THREADCOUNT)
 
-	fmt.Println("starting tasks")
-	// run 5 asynchronous tasks; each task will substract 1 from semaphore
-	go task(1, &wg, respond, stat, chstat)
-	go task(2, &wg, respond, stat, chstat)
-	go task(3, &wg, respond, stat, chstat)
-	go task(4, &wg, respond, stat, chstat)
-	go task(5, &wg, respond, stat, chstat)		
+	// set semaphore to x
+	wg.Add(THREADCOUNT)
+	
+	// run x asynchronous tasks; each task will substract 1 from semaphore
+	for i:=0;i<THREADCOUNT;i++{
+		go task(i, &wg, respond, stat)	
+	}
 
 	// waits until semaphore is zeroed
 	wg.Wait()
@@ -83,11 +77,11 @@ func main(){
 
 }
 
-func task(n int, wg *sync.WaitGroup, respond chan string, stat *Stat, chstat chan *Stat){
+func task(n int, wg *sync.WaitGroup, respond chan string, stat *Stat){
 	defer wg.Done()	// substracts 1 from the semaphore wg
 
 	time.Sleep(time.Duration(rand.Intn(10))*time.Second)
-	updateStat(stat, chstat, rand.Intn(10))
+	updateStat(stat, rand.Intn(10))
 
 	respond <- fmt.Sprintf("task %d is done", n)
 }
